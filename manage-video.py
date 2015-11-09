e047670995b56b15ba0840b236dafc999d2103d9
Jonathan Finlay <jfinlay@riseup.net>
2015-11-09 10:19:53 -0500

    Transpose and flip

--- manage-video.py
+++ manage-video.py
@@ -42,7 +42,14 @@ def capturefromcamera():
         else:
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
         # Display the resulting frame
-        framename = 'frame%s' % int(control / 2) or 1
+        framename = 'frame%s' % int(control / 3) or 1
+
+        # Flipping
+        if framecount % 4 == 0:
+            img = cv2.flip(img, 0)
+        # Transpose
+        if framecount % 3 == 0:
+            cv2.transpose(img)
         cv2.imshow(framename, img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -52,13 +59,36 @@ def capturefromcamera():
     cv2.destroyAllWindows()
 
 def saveinfile():
+    cap = cv2.VideoCapture(0)
+
+    #Define the codec and create ViweoWriter object
+    fourcc = cv2.cv.CV_FOURCC(*'XVID')
+    out = cv2.VideoWriter('capture/video/capture.avi', fourcc, 20.0, (640, 480))
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            flipcode = random.randint(-10, 10)
+            frame = cv2.flip(frame, flipcode)
+
+            # write flipped frame
+            out.write(frame)
+
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     return
 
 def capturefromfile():
-    return
+    print "No yet implemented"
 
-capturefromcamera()
-
-
+op = raw_input("(camera|save|file): ")
+if op == 'camera':
+    capturefromcamera()
+elif op == 'save':
+    saveinfile()
+elif op == 'file':
+    capturefromfile()
+else:
+    print 'Oops!!'
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
